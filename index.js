7a48d36f4940afa7b707cdcaf08be1dbd4c6a296
yash1258 <52569890+yash1258@users.noreply.github.com>
2022-11-23 12:52:45 +0530

    recommit

---
+++ index.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+
+const express = require('express');
+const app = express();
+const port = 8000;
+
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-startergy');
+//Mongo-store
+const MongoStore = require('connect-mongo')
+
+
+//cookie-parser
+const cookieParser = require('cookie-parser');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//database
+const db = require('./config/mongoose');
+//include layouts
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+
+//for styling static files
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+//static files
+app.use(express.static('./assets'));
+
+// EJS Set-up
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//MongoStore stores session cookies
+app.use(session({
+    name: 'MovieApp',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store : MongoStore.create(
+        {
+            mongoUrl : 'mongodb+srv://mullaaman0508:L%40tur123@cluster0.buvhlnw.mongodb.net/?retryWrites=true&w=majority',
+            autoRemove : 'disabled'
+        },function(err){
+            console.log(err || "Connection is fine");
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+app.use('/',require('./routes'));
+
+app.listen(port,function(err){
+    if(err){
+        console.log("Error: ",err);
+        return;
+    }
+    console.log("Successfully running on port",port);
+})
